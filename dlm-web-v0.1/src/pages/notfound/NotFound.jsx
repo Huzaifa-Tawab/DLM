a02912c3496a0a17ec5f04a007def285e002d360
Fasih <Fasih@softxion.com>
2024-01-18 17:38:55 +0500

    404 done

--- dlm-web-v0.1/src/pages/notfound/NotFound.jsx
+++ dlm-web-v0.1/src/pages/notfound/NotFound.jsx
@@ -1,7 +1,21 @@
 import React from "react";
-
+import "./notfound.css";
+import notfound from "../../Assets/notfound.jpg"
 function NotFound() {
-  return <div>NotFound</div>;
+  return <div className="not-found-sec">
+    <div className="not-found-cont">
+      <img src={notfound}></img>
+      <h1 className="cont---head"> Your Account is Blocked</h1>
+      <p className="cont---para">Please Contact with Admin</p>
+    </div>
+
+
+
+
+
+
+
+    </div>;
 }
 
 export default NotFound;
