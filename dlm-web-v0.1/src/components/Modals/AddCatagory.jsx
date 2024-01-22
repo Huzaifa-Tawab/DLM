0611b17ada25f1ae506a493c08fd5b15622cef37
Huzaifa <huzaifatawab33@gmail.com>
2024-01-22 16:12:17 +0500

    c

--- dlm-web-v0.1/src/components/Modals/AddCatagory.jsx
+++ dlm-web-v0.1/src/components/Modals/AddCatagory.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Modal from "simple-react-modal";
 
 import { db, storage } from "../../firebase";
-// import xIcon from "../../assets/Xincon.png";
+import xIcon from "../../assets/Xincon.png";
 import {
   addDoc,
   collection,
@@ -68,7 +68,7 @@ function AddCatagory({ showModal, onClose }) {
       <h2>Catagories</h2>
       <span>Once you save the item it can't be edit or delete</span>
       <div className="closebutton">
-        {/* <img onClick={onClose} src={xIcon} alt="" /> */}
+        <img onClick={onClose} src={xIcon} alt="" />
       </div>
       <div>
         <div className="modal-field-group">
