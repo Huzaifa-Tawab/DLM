e3f2a9fed5bc0d33b9dd30b4fe5b91ea2717a633
Fasih <Fasih@softxion.com>
2024-03-08 14:57:10 +0500

    page set customer page is now shown on webpage

--- dlm-web-v0.1/cnicFormatter.js
+++ dlm-web-v0.1/cnicFormatter.js
@@ -1,36 +1,15 @@
-// export default function cnicFormat(inputString) {
-//   // Split the string into parts using '-'
-//   var formatedCnic = "";
-
-//   // Check if the parts are present and their lengths
-//   for (const key in inputString) {
-//     if (key === "5" || key === "12") {
-//       formatedCnic += "-";
-//     }
-//     formatedCnic += inputString[key];
-//   }
-
-//   return formatedCnic;
-//  dont remove the above code }
-
 export default function cnicFormat(inputString) {
-  // Remove any existing '-' characters
-  const cnicWithoutHyphens = inputString.replace(/-/g, "");
+  // Split the string into parts using '-'
+  var formatedCnic = "";
 
-  // Check if the input is a valid CNIC (numeric with a length of 13 digits)
-  if (/^\d{13}$/.test(cnicWithoutHyphens)) {
-    // Format the CNIC with '-' after the first 5 digits and before the last digit
-    const formattedCnic =
-      cnicWithoutHyphens.substring(0, 5) +
-      "-" +
-      cnicWithoutHyphens.substring(5, 12) +
-      "-" +
-      cnicWithoutHyphens.charAt(12);
-
-    return formattedCnic;
-  } else {
-    // If the input is not a valid CNIC, return the original input
-
-    return inputString;
+  // Check if the parts are present and their lengths
+  for (const key in inputString) {
+    if (key === "5" || key === "12") {
+      formatedCnic += "-";
+    }
+    formatedCnic += inputString[key];
   }
+
+  return formatedCnic;
 }
+//  dont remove the above code
